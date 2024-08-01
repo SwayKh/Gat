d5b3644d2c36e37748f2fbe28add17fcbb125151
SwayKh <khatrisumit712@gmail.com>
2024-08-01 21:44:43 +0530

    first iteration, works with a single file provides with filepath

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Not enough arguments, Usage: gat [FILE]s...")
+		os.Exit(1)
+	}
+
+	f, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Fprintln(os.Stdout, string(f))
+
+	// for file := range os.Args[1:] {
+	// f, err := os.Open(os.Args[file])
+	// defer f.Close()
+
+	// scanner := bufio.NewScanner(f)
+	// for scanner.Scan() {
+	// 	fmt.Fprintln(os.Stdout, scanner.Text())
+	// }
+	// }
 }
 
 // TODO
-// Pull the license from git repo
-// Add codeblock to readme
+// Pull the license from git repo (DONE)
+// Add codeblock to readme (DONE)
 // Use os.READ to read, readfile() loads whole file in memory
 // Use bufio scanner to read file
 // Use for loop and read in increment for large files
