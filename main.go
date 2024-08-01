a4d34044164d430f8e293c0bb93c4b40fd0e7047
SwayKh <khatrisumit712@gmail.com>
2024-08-01 20:49:16 +0530

    Update README, Add To-do Items

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+}
+
+// TODO
+// Pull the license from git repo
+// Add codeblock to readme
+// Use os.READ to read, readfile() loads whole file in memory
+// Use bufio scanner to read file
+// Use for loop and read in increment for large files
+// Check for io.EOF to stop reading
+// Make is work with stdin ( send file to it with < )
+// Make is take multiple files
+// Check cat implementation and bat implementation
+// Check i billion row challenge to see what methods are used for performance
+// Print errors to stderr
