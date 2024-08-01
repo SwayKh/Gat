3ebd547db788db8b10c985cb2f164ac1042d6394
SwayKh <khatrisumit712@gmail.com>
2024-08-01 23:50:27 +0530

    Remove TODO comments, unneeded lines and comments, Hopefully everything works.

--- main.go
+++ main.go
@@ -22,19 +22,14 @@ func main() {
 func printFile(path string) error {
 	file, err := os.Open(path)
 	if err != nil {
-		// fmt.Fprintln(os.Stderr, err)
 		return fmt.Errorf("Error reading file: %s, %w", path, err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	// Read file line by line
-	scanner.Split(bufio.ScanLines)
 
-	// Returns a bool if newline character is found (I think)
 	for scanner.Scan() {
 		if err := scanner.Err(); err != nil {
-			// End the iteration if we encounter an error
 			return fmt.Errorf("Error reading file: %s, %w", path, err)
 		}
 		fmt.Fprintln(os.Stdout, scanner.Text())
@@ -47,17 +42,3 @@ func printFile(path string) error {
 	}
 	return nil
 }
-
-// TODO
-// Pull the license from git repo (DONE)
-// Add codeblock to readme (DONE)
-// Use os.READ to read, readfile() loads whole file in memory (Okay)
-// Use bufio scanner to read file (Done)
-// Use for loop and read in increment for large files (Used bool from
-// scanner.scan())
-// Check for io.EOF to stop reading (scanner.scan() stops on EOF)
-// Make is work with stdin ( send file to it with < ) (Dont want to)
-// Make is take multiple files (Done)
-// Check cat implementation and bat implementation
-// Check i billion row challenge to see what methods are used for performance
-// Print errors to stderr (Done)
